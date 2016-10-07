5e9dfe0903248e5da80f744d702d2cd48d2911e6
vishnu-nt <vishnunt5595@gmail.com>
2016-10-07 21:55:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content={
+var articleone={
     title:'article one| me',
     heading:'article one',
     date:'sep 5',
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one',function (req,res){
-  res.send(createTemplate(article-one))});
+  res.send(createTemplate(articleone))});
 
 app.get('/article-two',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'))});
