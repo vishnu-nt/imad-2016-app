1acc4cef101e5061a113d9a8111653d6b3a5e1ac
vishnu-nt <vishnunt5595@gmail.com>
2016-10-07 21:50:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,67 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content={
+    title:'article one| me',
+    heading:'article one',
+    date:'sep 5',
+    content: `<p>this is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the         contentthis is the content
+          </p>
+          <p>
+          this is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the content
+          </p>
+          <p>
+          this is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the contentthis is the content
+          </p>`
+};
+
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmltemplate=`
+    <html>
+    <head>
+        <title>
+            ${title}
+            </title>
+            <meta name="viemport" content="width=device-width, initial scale=1"/>
+       <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+  <body>
+      <div class="container">
+          <div>
+              <a href='/'>Home</a>
+          </div>
+          <br/>
+          <h3>
+              ${heading}
+          </h3>
+          <div>
+             ${date}
+          </div>
+          <div>
+              ${content}
+          </div>
+          </div>
+  </body>
+</html>
+`;
+return htmltemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
 app.get('/article-one',function (req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'))});
+  res.send(createtemplate(article-one))});
 
 app.get('/article-two',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'))});
