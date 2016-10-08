716f29da97fe67c605275d09c741c2b60757eb49
vishnu-nt <vishnunt5595@gmail.com>
2016-10-08 17:05:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img = document.getElementById('madi');
 marginLeft=0;
 function moveright(){
     marginLeft = marginLeft + 10;
-    img.style.marginLeft= arginLeft + 'px';
+    img.style.marginLeft= marginLeft + 'px';
 }
 img.onclick = function() {
     var interval = setInterval(moveright, 50);
