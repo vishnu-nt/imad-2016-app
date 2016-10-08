8e4e9126ec967d7bc62361a8c9ac6bc3a8c91f5e
vishnu-nt <vishnunt5595@gmail.com>
2016-10-08 16:52:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element= document.getElementById('maintext');
 element.innerHTML= 'new';
-var img= document.getElementById('madi');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginleft= '100px';
 };
