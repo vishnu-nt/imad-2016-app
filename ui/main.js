536b7ff83d28fab19deb56e978dd26d2571c028a
vishnu-nt <vishnunt5595@gmail.com>
2016-10-09 20:35:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,18 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function() {
     
-  counter = counter + 1;
-  var span = document.getElementById('count');
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if (request.readtstate === XMLHttpRequest.DONE){
+          if(request.status === 200){
+              var span = document.getElementById('count');
   span.innerHTML = counter.toString();
+              
+          }
+      }  
+    };
+ request.open('GET','http://vishnu-nt.imad.hasura-app.io/counter',true);
+ request.send(null);
+  
 };
