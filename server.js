3ea37701a63f4e86e2123a5b9dc224caa17b4a45
vishnu-nt <vishnunt5595@gmail.com>
2016-10-09 19:17:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ return htmltemplate;
 var counter= 0 ;
 app.get('/counter',function(req,res){
   counter=counter + 1;
-  res.send(counter.tostring());
+  res.send(counter.toString());
 });
 
 app.get('/', function (req, res) {
