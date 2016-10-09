9ff39681905427401ce1e586b8bf5eb97c0e56de
vishnu-nt <vishnunt5595@gmail.com>
2016-10-09 19:09:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,8 +72,11 @@ var htmltemplate=`
 `;
 return htmltemplate;
 }
-
-
+var counter= 0 ;
+app.get('/counter',function(req,res){
+  counter=counter + 1;
+  res.send(counter.tostring());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
